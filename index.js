cfe568b1e010722ec5622e7de2c87f0179616d11
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-12 15:13:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function logDriverNames(x){
+  x.map(function(e){
+    console.log(e.name)
+  })
+}
+
+
+function logDriversByHometown(x,y){
+  x.filter(function(v){
+    if (v.hometown== y){
+      console.log(v.name)
+    }
+  })
+}
+
+
+function driversByRevenue(drivers){
+  drivers.sort(function(a, b){
+      return a.revenue-b.revenue
+  })
+return drivers
+}
+
